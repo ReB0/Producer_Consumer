24a152747f1c62734c387177aec45675335655bd
ReB0 <andrea.rebolini@outlook.it>
2016-06-16 15:36:33 +0200

    Produttore_Consumatore with the use of interfaces and async methods

---
+++ Produttore-Consumatore_interfacce/Main.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace Produttore_Consumatore_interfacce
+{
+    class Producer_Consumer
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            int buffer_size = 0;
+            int n_consumers_rand = 0;   // number of random consumers
+            int n_producers_rand = 0;   // number of random producers
+            int n_producers_seq = 0;    // number of sequential producers
+
+
+            Console.Write("Insert the buffer size: ");
+            buffer_size = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Insert the number of random producers: ");
+            n_producers_rand = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.Write("Insert the number of sequential producers: ");
+            n_producers_seq = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Insert the numer of random consumers: ");
+            n_consumers_rand = Convert.ToInt32(Console.ReadLine());
+
+            
+            Buffer.Buffer_Size(buffer_size); 
+
+
+            List<Produttore_Random> producers_rand = new List<Produttore_Random>();
+            List<Consumatore_Random> consumers_rand = new List<Consumatore_Random>();
+            List<Produttore_Sequenziale> producers_seq = new List<Produttore_Sequenziale>();
+
+
+            for (int i = 0; i < n_producers_rand; i++) { producers_rand.Add(new Produttore_Random(i, buffer_size)); }
+            for (int j = 0; j < n_consumers_rand; j++) { consumers_rand.Add(new Consumatore_Random(j, buffer_size)); }
+            for (int k = 0; k < n_producers_seq; k++) { producers_seq.Add(new Produttore_Sequenziale(k, buffer_size)); }
+            
+            // Creo i vari thread, uno per ciascun produttore/consumatore
+
+            Thread[] thread = new Thread[n_consumers_rand + n_producers_rand+n_producers_seq];
+            
+            //inizializzo l'operazione che dovrà compiere ciascun thread
+
+            for (int i = 0; i < n_producers_rand; i++)
+            {
+                thread[i] = new Thread(new ThreadStart(producers_rand.ElementAt(i).Start));
+            }
+
+
+            for (int j = 0; j < n_consumers_rand; j++)
+            {
+                thread[j+n_producers_rand] = new Thread(new ThreadStart(consumers_rand.ElementAt(j).Start));
+            }
+
+            for (int k = 0; k < n_producers_seq; k++)
+            {
+                thread[k+n_producers_rand+n_consumers_rand] = new Thread(new ThreadStart(producers_seq.ElementAt(k).Start));
+            }
+
+
+            //Do l'avvio a tutti i thread
+            foreach (Thread x in thread)
+            {
+                x.Start();
+            }
+            Console.Read();
+
+        }
+
+
+    }
+}
---
+++ Produttore-Consumatore_interfacce/Utenti.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace Produttore_Consumatore_interfacce
+{
+
+
+    abstract class Utenti
+    {
+        public int id { get; set; }  // identificativo dell'utente che accede al buffer
+        protected Random rand = new Random((int)DateTime.Now.Ticks);   // generatore di numeri casuali
+        
+        // protected abstract void Method1(); 
+       
+    }
+    
+
+    class Produttore_Random : Utenti, IProduttore {
+
+        private int value;
+        private int buf_size;
+        private int in_buffer;
+
+        public Produttore_Random(int idx, int bfsize) {
+            id = idx;
+            buf_size = bfsize;
+        }
+
+        public void Start() {
+
+            Produci();
+
+        }
+
+        public async void Produci()
+        {
+
+            while (true)
+            {
+
+                Thread.Sleep(new Random(new Random().Next(500)).Next(700, 1400));
+                
+                value = rand.Next(1000);
+
+                lock (typeof(Buffer))
+                {
+                    in_buffer = Buffer.Write(value);
+                    Stampa_Random_Producer(id, in_buffer);
+                }
+
+                if (in_buffer == buf_size) { await Task.Run(() => Buffer.Buffer_is_not_full()); }
+                
+            }
+
+
+        }
+
+        static public void Stampa_Random_Producer(int id, int in_buffer){
+            
+         Console.ForegroundColor = ConsoleColor.DarkYellow;
+         Console.Write("Random producer {0} wrote into buffer. {1} values stored in buffer.\n", id, in_buffer);
+         Console.ResetColor();
+
+        }
+
+
+
+        }
+
+   
+     class Consumatore_Random : Utenti,IConsumatore{
+
+        private int in_buffer;
+        private int buf_size;
+
+    public Consumatore_Random(int idx, int bfsize) {
+         id = idx;
+         buf_size = bfsize;
+
+        }
+
+        public void Start()
+        {
+             Consuma();
+        }
+
+        public async void Consuma() {
+
+        while (true)
+        {
+            Thread.Sleep(new Random(new Random().Next(500)).Next(700, 1400));
+                
+                lock (typeof(Buffer))
+                {
+                  in_buffer = Buffer.Read();
+                  Stampa_Random_Consumer(id,in_buffer);
+                }
+                
+                if (in_buffer == 0) { await Task.Run(() => Buffer.Buffer_is_not_empty()); }
+
+            }
+
+        }
+
+        static public void Stampa_Random_Consumer(int id, int in_buffer)
+        {
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("Random consumer {0} read from buffer. {1} values stored in buffer.\n", id, in_buffer);
+            Console.ResetColor();
+
+        }
+        
+        
+    }
+    
+
+   class Produttore_Sequenziale: IProduttore{
+
+        private int value;
+        private int id;  
+        private int buf_size;
+        private int in_buffer;
+
+        public Produttore_Sequenziale(int idx, int bfsize) {
+            id = idx;
+            buf_size = bfsize;
+        }
+
+        public void Start()
+        {
+           Produci();
+        }
+        
+        public async void Produci(){
+
+
+            while (true)
+            {
+                Thread.Sleep(new Random(new Random().Next(500)).Next(700, 1400));
+                
+                    lock (typeof(Buffer))
+                     {
+                    value = value + 1;
+                    in_buffer = Buffer.Write(value);
+                    Stampa_Sequential_Producer(id, in_buffer);
+                    }
+
+                if (in_buffer == buf_size) { await Task.Run(() => Buffer.Buffer_is_not_full()); }
+                
+            }
+        }
+
+        static public void Stampa_Sequential_Producer(int id, int in_buffer)
+        {
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Sequential producer {0} wrote into buffer. {1} values stored in buffer.", id, in_buffer);
+            Console.ResetColor();
+
+        }
+        
+
+    }
+
+
+}
